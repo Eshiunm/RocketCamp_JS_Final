9b09d840445d5af8c9c0e5812b899975e35d1286
Mosi <mosisu07@gmail.com>
2024-03-11 16:40:42 +0800

    refactor: modified API files, deleted all.js.

--- src/script/api/products.js
+++ src/script/api/products.js
@@ -1,2 +1,12 @@
 import path from './apiNext.js';
 
+function products(token) {
+  axios
+    .get(path.products(token))
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
